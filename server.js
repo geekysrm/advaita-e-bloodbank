60f4a28d0ff9f95e83d449f24391faebb1c632b4
sonu <sonusaikishan@gmail.com>
2018-02-11 15:44:58 +0530

    changed server.js

--- server.js
+++ server.js
@@ -7,19 +7,15 @@ var crypto = require('crypto');
 
 var XMLHttpRequest = require('xhr2');
 
-
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
    extended: false
 }));
 
-
-
 app.get('/',function(req,res){
   res.send('Welcome to new app!');
 });
-
 
 app.listen(5000, function () {
   console.log('Example app listening on port 5000!');
