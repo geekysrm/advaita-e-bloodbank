c3ca2dc776409d01ac8f579cae3ba9db1760b3a2
Soumya <soumyacool2012@gmail.com>
2018-02-11 15:36:06 +0530

    Major changes in setup1

---
+++ client/src/Welcome1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Welcome = () => (
+    <div>
+
+            <h1 >Welcome to App</h1>
+            
+      </div>
+   
+);
+
+export default Welcome;
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var express = require('express');
 var app = express();
 var router = express.Router();
@@ -9,41 +8,19 @@ var crypto = require('crypto');
 var XMLHttpRequest = require('xhr2');
 
 
-=======
-const express = require('express');
-const app = express();
-
->>>>>>> 05b24fc77130a97333521b8aa870af53e8ba67b9
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
    extended: false
 }));
-<<<<<<< HEAD
-=======
+
+
 
 app.get('/',function(req,res){
-  res.send("hello");
+  res.send('Welcome to new app!');
 });
 
-app.get('/api/customers', (req, res) => {
-  const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
-  ];
->>>>>>> 05b24fc77130a97333521b8aa870af53e8ba67b9
-
-
-
-<<<<<<< HEAD
-app.get('/',function(req,res){
-  res.send('Welcome!');
-});
 
 app.listen(5000, function () {
   console.log('Example app listening on port 5000!');
 });
-=======
-app.listen(port, () => `Server running on port ${port}`);
->>>>>>> 05b24fc77130a97333521b8aa870af53e8ba67b9
