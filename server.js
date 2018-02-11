05b24fc77130a97333521b8aa870af53e8ba67b9
sonu <sonusaikishan@gmail.com>
2018-02-11 15:04:21 +0530

    changed server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
-
 const app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+   extended: false
+}));
+
+app.get('/',function(req,res){
+  res.send("hello");
+});
 
 app.get('/api/customers', (req, res) => {
   const customers = [
