7db1c2301719a10444a9810619a408773e99b2c7
sonu <sonusaikishan@gmail.com>
2018-02-11 23:25:14 +0530

    added /add-doner api endpoint

--- server.js
+++ server.js
@@ -5,6 +5,19 @@ var path = require('path');
 
 var crypto = require('crypto');
 
+//for database
+var Pool = require('pg').Pool;
+
+
+var config = {
+  host: 'localhost',
+  user: 'danglingpointers',
+  password: 'srm52sai24',
+  database: 'attendancemanager'
+};
+
+var pool = new Pool(config);
+
 var XMLHttpRequest = require('xhr2');
 
 var bodyParser = require('body-parser');
@@ -15,6 +28,36 @@ app.use(bodyParser.urlencoded({
 
 app.get('/',function(req,res){
   res.send('Welcome to new app!');
+});
+
+app.post('/add-doner',function(req,res){
+
+  var nm = req.body.name;
+  var age1 = req.body.age;
+  var location = req.body.place;
+  var gen = req.body.gender;
+  var blood_grp = req.body.blood_group;
+
+  pool.query(`INSERT INTO "doners" ("name", "age", "place", "gender", "blood_group")
+              VALUES ($1, $2, $3, $4, $5);`,[nm,age1,location,gen,blood_grp],function(err,result){
+    if(err)
+    {
+      res.status(500).send(err.toString());
+    }
+    else
+    {
+      res.status(200).send("insert succesfull");
+    }
+  });
+
+});
+
+app.post('/get-doners',function(req,res){
+
+  var blood_grp = req.body.blood_group;
+
+  
+
 });
 
 app.listen(5000, function () {
